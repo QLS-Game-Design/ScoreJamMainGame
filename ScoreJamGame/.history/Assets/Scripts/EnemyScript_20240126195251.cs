2c0e85ae63419aee60d6505c5a3fabf0f7cba79d
F.H <franklinhu2007@gmail.com>
2024-01-26 20:00:52 -0800

    enemy
    it follow

---
+++ ScoreJamGame/.history/Assets/Scripts/EnemyScript_20240126195251.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+    private float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance =  Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+
+        transform.position-= Vector2
+    }
+}
